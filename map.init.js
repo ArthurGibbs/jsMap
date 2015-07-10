667a6a4de7528f96064269d09d186a97bdd84b01
Arthur Gibbs <Arthur.gibbs@diamond.ac.uk>
2015-07-10 17:09:02 +0100

    hide roads, colour , to back roofs

--- map.init.js
+++ map.init.js
@@ -1,27 +1,18 @@
 jQuery(function($) {
     var inDetails = false;
     var container = $("#map");
-
-    var r = Raphael('map', container.width()* 10, container.height()* 10);
+    var r = Raphael('map', 5000, 5000);
     var panZoom = r.panzoom({
-        initialZoom: 6,
+        initialZoom: 2,
+		//maxZoom: 12,
         initialPosition: {
-            x: 120,
-            y: 70
+            x: 271,
+            y: 285
         }
     });
     var isHandling = false;
-
     panZoom.enable();
     r.safari();
-
-    var attributes = {
-        fill: '#F1F1F1',
-        stroke: '#FFFFFF',
-        'stroke-width': 2,
-        'stroke-linejoin': 'round'
-    };
-
     var overlay = r.rect(0, 0, r.width, r.height);
     overlay.attr({
         fill: '#ffffff',
@@ -29,86 +20,111 @@ jQuery(function($) {
         "stroke-width": 0,
         stroke: '#ffffff'
     });
-
     var countries = [];
     var i = 0;
 	
-	
-		$.get("map.svg", function(d){
-			$(d).find('path').each(function(){			
-				var $book = $(this); 
-				var description = $book.find('desc').text(); 
-				var dat = $book.attr("d");
-				var id = $book.attr("id");
-				var style = $book.attr("style");
-
-				var singularInstance = {};
-				singularInstance.path = dat;
-				singularInstance.name = id;
-				singularInstance.detail = description;
-
-				singularInstance.objectp = placeObject(singularInstance);
-				countries[i] = singularInstance;		
-				placeButton(singularInstance);		
-				i++;
+	$.get("map.svg", function(d){
+		$(d).find('path').each(function(){			
+			var $book = $(this); 
+			var description = $book.find('desc').text(); 
+			var title = $book.find('title').text(); 
+			var dat = $book.attr("d");
+			var id = $book.attr("id");
+			var style = $book.attr("style");
+			var singularInstance = {};
+			singularInstance.path = dat;
+			singularInstance.name = id;
+			
+			var rx = /^([^\[]*)/gm;
+			var arr = rx.exec(title); 
+			if (arr != null && arr[1]) {
+				singularInstance.title = arr[1];	
 				
-			});
-			$(".myButton").click(doButtonPress);
-		});	
+			}
+			
+			rx = /\[(.*)\]/gm;
+			var arr = rx.exec(title); 
+			if (arr != null && arr[1]) {
+				singularInstance.options = arr[1];					
+			}
 			
 			
-  //  for (var country in paths) {
-	//	var singularInstance = {};
-	//	singularInstance.path = paths[country].path;
-	//	singularInstance.name = paths[country].name;       
-	//	singularInstance.objectp = placeObject(singularInstance);
-   //     countries[i] = singularInstance;		
-	//	placeButton(singularInstance);		
-  //      i++;
-  //  }
+			singularInstance.fill = id;
+			singularInstance.detail = description;
+			singularInstance.style = style;
+			singularInstance.objectp = placeObject(singularInstance);
+			countries[i] = singularInstance;
+			if (singularInstance.title){
+				placeButton(singularInstance);		
+			}
+			i++;
+			
+		});
+		$(".myButton").click(doButtonPress);
+	});	
 	
 	function placeButton(instance) {
 		var element = document.createElement("input");
         //Assign different attributes to the element. 
         element.type = "button";
-        element.value = instance.name;
-        element.name = instance.name;
+        element.value = instance.title;
+        element.name = instance.title;
         element.className = "myButton";
         element.setAttribute('data-id', i)
         infolist.appendChild(element);
 	}
 	
 	function placeObject(instance) {
+				
+		var attributes = {
+			fill: '#F1F1F1',
+			stroke: '#000000',
+			'stroke-width': 1,
+			'stroke-linejoin': 'round'
+		};
 		var obj = r.path(instance.path);
+		
+		var rx = /.*fill\:(#[a-z0-9]+);/gm;
+		var arr = rx.exec(instance.style); 
+		if (arr != null && arr[1] != null) {
+			attributes.fill = arr[1];
+			obj.data("fill", arr[1]);
+		} else {
+			obj.data("fill", "#F1F1F1");	
+		}
+		
         obj.attr(attributes);
-        obj.click(handleDetails);
-        obj.data("hoverFill", "#3e5f43");
-        obj.data("fill", "#F1F1F1");
+        obj.click(handleShapeClick);
+		obj.data("hoverFill", "#FA1167");
+		
+		
+		var ops = instance.options;
+		if (!ops || !ops.includes("h")){
+			obj.hover(animateOver, animateOut);		
+		}
 		obj.data("id", i);
-       // obj.data("name", instance.name)
-        obj.hover(animateOver, animateOut);	
+		
 		return obj 
 	}
 	
 	var lastobject = null;
     var doButtonPress = function() {
         var country = countries[$(this).data("id")];
-        //alert(country.name);
-        //moveto(country.centerpos.x, country.centerpos.y)
-  
-  
-		if (lastobject != null) {
-			lastobject.objectp.attr("fill", lastobject.objectp.data("fill"));	
-		}
+		
+		clearlastHighlight();
         if (country.objectp.data("hoverFill")) {
             country.objectp.attr("fill", country.objectp.data("hoverFill"));
         }
 		lastobject = country;
 		displayDetails(country);
     }
-
+	
+	function clearlastHighlight() {
+			if (lastobject != null) {
+			lastobject.objectp.attr("fill", lastobject.objectp.data("fill"));	
+		}			
+	}
     
-
     function pathtocenter(path) {
         //needs improvement
         var out = {};
@@ -121,36 +137,28 @@ jQuery(function($) {
         //alert(out.x + "," + out.y);	
         return out
     }
-
     function moveto(x, y) {
-        //alert(x +","+ y)
         var currentpos = panZoom.getCurrentPosition();
         panZoom.pan(x - currentpos.x, y - currentpos.y);
         var currPaperZoom = panZoom.getCurrentZoom();
-        //alert(currPaperZoom);	
-        //panZoom.zoomIn(10 - currPaperZoom);
-        //panZoom.enable();
     }
-
     $("#mapContainer #up").click(function(e) {
         panZoom.zoomIn(1);
         e.preventDefault();
     });
-
     $("#mapContainer #down").click(function(e) {
         panZoom.zoomOut(1);
         e.preventDefault();
     });
-
     function animateOver() {
+		clearlastHighlight();
 		displayDetails(countries[this.data("id")]);
         if (this.data("hoverFill")) {
             this.attr("fill", this.data("hoverFill"));
         }
     }
-
     function animateOut() {
-		 displayDetails(null);
+		displayDetails(null);
         if (this.data("fill")) {
             this.attr("fill", this.data("fill"));
         }
@@ -158,7 +166,7 @@ jQuery(function($) {
 	
 	function displayDetails(obj) {
 		if (obj != null) {
-			document.getElementById("infoname").innerHTML = obj.name;	
+			document.getElementById("infoname").innerHTML = obj.title;	
 			document.getElementById("infodetail").innerHTML = obj.detail;	
 		} 
 		else {
@@ -166,15 +174,15 @@ jQuery(function($) {
 			document.getElementById("infodetail").innerHTML = "";		
 		}
 	}
-
-    function handleDetails() {
-        displayDetails(countries[this.data("id")]);
-        var currentpos = panZoom.getCurrentPosition();
-		alert(currentpos.x);
-
-        // need to re enable panzoom for some reason
+    function handleShapeClick() {
+		var cntry = countries[this.data("id")];
+        displayDetails(cntry);
+		
+		if (cntry.options != null && cntry.options.includes("b")) {
+			//alert("bk");
+			
+			this.toBack();
+		}
         panZoom.enable();
-
     }
-
 });
