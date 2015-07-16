58a79c6f5af7904ae4ee5f2f8402817a86761167
Arthur Gibbs <Arthur.gibbs@diamond.ac.uk>
2015-07-16 09:20:54 +0100

    roof reatures

--- map.init.js
+++ map.init.js
@@ -4,7 +4,8 @@ jQuery(function($) {
     var r = Raphael('map', 5000, 5000);
     var panZoom = r.panzoom({
         initialZoom: 2,
-		//maxZoom: 12,
+ 
+        minZoom: 1,
         initialPosition: {
             x: 271,
             y: 285
@@ -16,132 +17,197 @@ jQuery(function($) {
     var overlay = r.rect(0, 0, r.width, r.height);
     overlay.attr({
         fill: '#ffffff',
-        'fill-opacity': 0,
+        'fill-opacity': 0.5,
         "stroke-width": 0,
         stroke: '#ffffff'
     });
-    var countries = [];
-    var i = 0;
-	
-	$.get("map.svg", function(d){
-		$(d).find('path').each(function(){			
-			var $book = $(this); 
-			var description = $book.find('desc').text(); 
-			var title = $book.find('title').text(); 
-			var dat = $book.attr("d");
-			var id = $book.attr("id");
-			var style = $book.attr("style");
-			var singularInstance = {};
-			singularInstance.path = dat;
-			singularInstance.name = id;
-			
-			var rx = /^([^\[]*)/gm;
-			var arr = rx.exec(title); 
-			if (arr != null && arr[1]) {
-				singularInstance.title = arr[1];	
-				
-			}
-			
-			rx = /\[(.*)\]/gm;
-			var arr = rx.exec(title); 
-			if (arr != null && arr[1]) {
-				singularInstance.options = arr[1];					
-			}
-			
-			
-			singularInstance.fill = id;
-			singularInstance.detail = description;
-			singularInstance.style = style;
-			singularInstance.objectp = placeObject(singularInstance);
-			countries[i] = singularInstance;
-			if (singularInstance.title){
-				placeButton(singularInstance);		
-			}
-			i++;
-			
-		});
-		$(".myButton").click(doButtonPress);
-	});	
-	
-	function placeButton(instance) {
-		var element = document.createElement("input");
+
+    var mapObjects = [];
+    var rotationTimer;
+    var lastSelectedObject = null;
+    var p1 = null;
+    var p2 = null;
+
+    function pointTo(p, x, y) {
+        var path = "m " + x + "," + y + " 5,30 5,-10 10,0 -20,-20 z";
+        if (p !== null) {
+            p.remove();
+        }
+        p = r.path(path);
+        var attributes = {
+            fill: 'FF0080',
+            stroke: '#000000',
+            'stroke-width': 1,
+            'stroke-linejoin': 'round'
+        };
+        p.data("fill", "#F1F1F1");
+        p.attr(attributes);
+    }
+
+    $.get("map.svg", function(d) {
+        var i = 0;
+        $(d).find('path').each(function() {
+            var $book = $(this);
+            var description = $book.find('desc').text();
+            var title = $book.find('title').text();
+            var dat = $book.attr("d");
+            var id = $book.attr("id");
+            var style = $book.attr("style");
+            var singularInstance = {};
+            singularInstance.path = dat;
+            singularInstance.name = id;
+            singularInstance.id = i;
+            var rx = /^([^\[]*)/gm;
+            var arr = rx.exec(title);
+            if (arr !== null && arr[1]) {
+                singularInstance.title = arr[1];
+            }
+
+            rx = /\[(.*)\]/gm;
+            arr = rx.exec(title);
+            if (arr !== null && arr[1]) {
+                singularInstance.options = arr[1];
+            }
+
+            singularInstance.fill = id;
+            singularInstance.detail = description;
+            singularInstance.style = style;
+            singularInstance.objectp = placeObject(singularInstance);
+
+            singularInstance.pos = {};
+            singularInstance.pos.x = singularInstance.objectp.getBBox().x + (singularInstance.objectp.getBBox().width / 2);
+            singularInstance.pos.y = singularInstance.objectp.getBBox().y + (singularInstance.objectp.getBBox().height / 2);
+
+            mapObjects[i] = singularInstance;
+            if (singularInstance.title && !(singularInstance.options && singularInstance.options.includes("o"))) {
+                placeButton(singularInstance);
+            }
+            i++;
+
+        });
+        $(".myButton").click(doButtonPress);
+    });
+
+    function rotate() {
+        if (mapObjects.length > 0) {
+            for (var i = 0; i <= mapObjects.length; i++) {
+                if (mapObjects[i] && mapObjects[i].options) {
+                    if (mapObjects[i].options.includes("R")) {
+                        mapObjects[i].objectp.rotate(8);
+                    }
+                }
+            }
+        }
+        rotationTimer = setTimeout(rotate, 20);
+    }
+
+    rotationTimer = setTimeout(rotate, 20);
+
+
+    function placeButton(instance) {
+        var element = document.createElement("input");
         //Assign different attributes to the element. 
         element.type = "button";
         element.value = instance.title;
         element.name = instance.title;
         element.className = "myButton";
-        element.setAttribute('data-id', i)
+        element.setAttribute('data-id', instance.id);
         infolist.appendChild(element);
-	}
-	
-	function placeObject(instance) {
-				
-		var attributes = {
-			fill: '#F1F1F1',
-			stroke: '#000000',
-			'stroke-width': 1,
-			'stroke-linejoin': 'round'
-		};
-		var obj = r.path(instance.path);
-		
-		var rx = /.*fill\:(#[a-z0-9]+);/gm;
-		var arr = rx.exec(instance.style); 
-		if (arr != null && arr[1] != null) {
-			attributes.fill = arr[1];
-			obj.data("fill", arr[1]);
-		} else {
-			obj.data("fill", "#F1F1F1");	
-		}
-		
+    }
+
+    function placeObject(instance) {
+
+        var attributes = {
+            fill: '#F1F1F1',
+            stroke: '#000000',
+            'stroke-width': 1,
+            'stroke-linejoin': 'round'
+        };
+        var obj = r.path(instance.path);
+
+        var rx = /.*fill\:(#[a-z0-9]+);/gm;
+        var arr = rx.exec(instance.style);
+        if (arr !== null && arr[1] !== null) {
+            attributes.fill = arr[1];
+            obj.data("fill", arr[1]);
+            obj.data("original-fill", arr[1]);
+        } else {
+            //obj.data("fill", "#F1F1F1");
+        }
+
         obj.attr(attributes);
         obj.click(handleShapeClick);
-		obj.data("hoverFill", "#FA1167");
-		
-		
-		var ops = instance.options;
-		if (!ops || !ops.includes("h")){
-			obj.hover(animateOver, animateOut);		
-		}
-		obj.data("id", i);
-		
-		return obj 
-	}
-	
-	var lastobject = null;
-    var doButtonPress = function() {
-        var country = countries[$(this).data("id")];
-		
-		clearlastHighlight();
-        if (country.objectp.data("hoverFill")) {
-            country.objectp.attr("fill", country.objectp.data("hoverFill"));
+        obj.data("hoverFill", "#FA1167");
+
+
+        var ops = instance.options;
+        if (!ops || !ops.includes("h")) {
+            obj.hover(animateOver, animateOut);
         }
-		lastobject = country;
-		displayDetails(country);
+        obj.data("id", instance.id);
+
+        return obj
     }
-	
-	function clearlastHighlight() {
-			if (lastobject != null) {
-			lastobject.objectp.attr("fill", lastobject.objectp.data("fill"));	
-		}			
-	}
-    
-    function pathtocenter(path) {
-        //needs improvement
-        var out = {};
-        var a = path.replace(/[a-zA-Z ]+/g, "|");
-        var b = a.split("|");
-        var c = b[1].split(",");
-        out.x = c[0];
-        out.y = c[1].
-        split(" ")[0];
-        //alert(out.x + "," + out.y);	
-        return out
+
+
+    var doButtonPress = function() {
+        var mapObject = mapObjects[$(this).data("id")];
+
+        mapObject.objectp.toFront();
+        mapObject.objectp.data("fill", mapObject.objectp.data("original-fill"));
+
+        clearlastHighlight();
+        mapObject.objectp.attr({
+            "fill-opacity": 1
+        });
+        if (mapObject.objectp.data("hoverFill")) {
+            mapObject.objectp.attr("fill", mapObject.objectp.data("hoverFill"));
+        }
+        lastSelectedObject = mapObject;
+        displayDetails(mapObject);
+
+
+
+        zoomTo(mapObject);
+
+
+
+        possibleOpenParentRoof(mapObject);
     }
-    function moveto(x, y) {
-        var currentpos = panZoom.getCurrentPosition();
-        panZoom.pan(x - currentpos.x, y - currentpos.y);
-        var currPaperZoom = panZoom.getCurrentZoom();
+
+    function zoomTo(mapObject) {
+
+        var currentPos = panZoom.getCurrentPosition();
+        alert("current:" + currentPos.x + "," + currentPos.y);
+
+
+        alert("object:" + mapObject.pos.x + "," + mapObject.pos.y);
+
+
+
+
+        //alert(panZoom.getCurrentZoom());
+        panZoom.zoomIn(7 - panZoom.getCurrentZoom());
+
+        var difx = mapObject.pos.x - currentPos.x;
+        var dify = mapObject.pos.y - currentPos.y;
+
+        //alert("diff:" + difx + "," + dify);
+
+
+        panZoom.pan(difx * 0.03, dify * 0.03);
+        //panZoom.pan(10, 10);
+        pointTo(p2, difx, dify);
+
     }
+
+    function clearlastHighlight() {
+        if (lastSelectedObject !== null) {
+            lastSelectedObject.objectp.attr("fill", lastSelectedObject.objectp.data("fill"));
+        }
+    }
+
+
     $("#mapContainer #up").click(function(e) {
         panZoom.zoomIn(1);
         e.preventDefault();
@@ -150,41 +216,72 @@ jQuery(function($) {
         panZoom.zoomOut(1);
         e.preventDefault();
     });
+
     function animateOver() {
-		clearlastHighlight();
-		displayDetails(countries[this.data("id")]);
-        if (this.data("hoverFill")) {
-            this.attr("fill", this.data("hoverFill"));
+        clearlastHighlight();
+        var mapObject = mapObjects[this.data("id")];
+        if (mapObject.title) {
+            displayDetails(mapObject);
+            if (this.data("hoverFill")) {
+                this.attr("fill", this.data("hoverFill"));
+            }
         }
     }
+
     function animateOut() {
-		displayDetails(null);
+        displayDetails(null);
         if (this.data("fill")) {
             this.attr("fill", this.data("fill"));
         }
     }
-	
-	function displayDetails(obj) {
-		if (obj != null) {
-			document.getElementById("infoname").innerHTML = obj.title;	
-			document.getElementById("infodetail").innerHTML = obj.detail;	
-		} 
-		else {
-			document.getElementById("infoname").innerHTML = "";	
-			document.getElementById("infodetail").innerHTML = "";		
-		}
-	}
+
+    function displayDetails(obj) {
+        if (obj !== null) {
+            document.getElementById("infoname").innerHTML = obj.title;
+            document.getElementById("infodetail").innerHTML = obj.detail;
+        } else {
+            document.getElementById("infoname").innerHTML = "";
+            document.getElementById("infodetail").innerHTML = "";
+        }
+    }
+
     function handleShapeClick() {
-		var cntry = countries[this.data("id")];
-        displayDetails(cntry);
-		
-		if (cntry.options != null && cntry.options.includes("b")) {
-			//alert("bk");
-	
-			this.toBack();
-			this.attr("fill", "#595959");
-			this.data("fill", "#595959");	
-		}
+        var mapObject = mapObjects[this.data("id")];
+        displayDetails(mapObject);
+        possibleOpenRoof(mapObject);
+        possibleOpenParentRoof(mapObject);
         panZoom.enable();
+    }
+
+    function possibleOpenParentRoof(mapObject) {
+        var rx = /.*[B]\=([0-9])+.*/gm;
+        var arr = rx.exec(mapObject.options);
+        if (arr !== null && arr[1] !== null) {
+            for (var i = 1; i <= arr.length; i++) {
+                openRoof(arr[i]);
+            }
+        }
+    }
+
+    function possibleOpenRoof(mapObject) {
+        var rx = /.*[b]\=([0-9])+.*/gm;
+        var arr = rx.exec(mapObject.options);
+        if (arr !== null && arr[1] !== null) {
+            openRoof(arr[1]);
+        }
+    }
+
+    function openRoof(id) {
+        if (mapObjects.length > 0) {
+            for (var i = 0; i <= mapObjects.length; i++) {
+                if (mapObjects[i] && mapObjects[i].options) {
+                    if (mapObjects[i].options.includes("b=" + id)) {
+                        mapObjects[i].objectp.toBack();
+                        mapObjects[i].objectp.attr("fill", "#777777");
+                        mapObjects[i].objectp.data("fill", "#777777");	
+                    }
+                }
+            }
+        }
     }
 });
