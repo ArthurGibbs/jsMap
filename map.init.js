3e52cae46ad8a6551050346636046a6a5332f8c8
Arthur Gibbs <Arthur.gibbs@diamond.ac.uk>
2015-07-21 12:46:09 +0100

    replaced search, made firefox complient

--- map.init.js
+++ map.init.js
@@ -1,125 +1,98 @@
 jQuery(function($) {
-    var inDetails = false;
     var container = $("#map");
     var r = Raphael('map', 5000, 5000);
     var panZoom = r.panzoom({
         initialZoom: 2,
- 
         minZoom: 1,
         initialPosition: {
             x: 94,
             y: 213
         }
     });
-    var isHandling = false;
     panZoom.enable();
     r.safari();
-    var overlay = r.rect(0, 0, r.width, r.height);
-    overlay.attr({
-        fill: '#ffffff',
-        'fill-opacity': 0.5,
-        "stroke-width": 0,
-        stroke: '#ffffff'
-    });
 
     var mapObjects = [];
     var rotationTimer;
     var lastSelectedObject = null;
+	var fuseSearchBook = {};
+
+	//Disabled due to causing crash? unsolved
+	//rotationTimer = setTimeout(rotate, 20);
 
 	
-	var fussyNames = {};
-	var clearNames = [];
-	rotationTimer = setTimeout(rotate, 20);
-    function pointTo(p, x, y) {
-        var path = "m " + x + "," + y + " 5,30 5,-10 10,0 -20,-20 z";
-        if (p !== null) {
-            p.remove();
-        }
-        p = r.path(path);
-        var attributes = {
-            fill: 'FF0080',
-            stroke: '#000000',
-            'stroke-width': 1,
-            'stroke-linejoin': 'round'
-        };
-        p.data("fill", "#F1F1F1");
-        p.attr(attributes);
-    }
-
     $.get("data/map.svg", function(d) {
         var i = 0;
         $(d).find('path').each(function() {
+			// Extract from SVG
             var $book = $(this);
-            var description = $book.find('desc').text();
-            var title = $book.find('title').text();
-            var dat = $book.attr("d");
-            var id = $book.attr("id");
-            var style = $book.attr("style");
+            var objectRawTitle = $book.find('title').text();				
             var singularInstance = {};
-            singularInstance.path = dat;
-            singularInstance.name = id;
             singularInstance.id = i;
-            var rx = /^([^\[]*)/gm;
-            var arr = rx.exec(title);
-			singularInstance.title = null;
-            if (arr !== null && arr[1]) {
-                singularInstance.title = arr[1];
-            }
-
-            rx = /\[(.*)\]/gm;
-            arr = rx.exec(title);
-            if (arr !== null && arr[1]) {
-                singularInstance.options = arr[1];
-            }
-
-            singularInstance.fill = id;
-            singularInstance.detail = description;
-            singularInstance.style = style;
+            singularInstance.options = getOptionsFromString(objectRawTitle);
+			singularInstance.title = getTitleFromString(objectRawTitle);
+            singularInstance.description = $book.find('desc').text();;
+            singularInstance.path = $book.attr("d");
+            singularInstance.objectName = $book.attr("id");			
+            singularInstance.style = $book.attr("style");
             singularInstance.objectp = placeObject(singularInstance);
-
-            singularInstance.pos = {};
-            singularInstance.pos.x = singularInstance.objectp.getBBox().x + (singularInstance.objectp.getBBox().width / 2);
-            singularInstance.pos.y = singularInstance.objectp.getBBox().y + (singularInstance.objectp.getBBox().height / 2);
-
-			//alert("got");
-			if (singularInstance.title != null){
-				var stringtitle = singularInstance.title;
-				clearNames.push(stringtitle);
-			}
-            mapObjects[i] = singularInstance;
-            if (singularInstance.title && !(singularInstance.options && singularInstance.options.includes("o"))) {
-                placeButton(singularInstance);
-            }
+			singularInstance.pos = getPos(singularInstance);       
+			
+            mapObjects[i] = singularInstance;		
             i++;
-
         });
-       
 		
-		
-		fussyNames = FuzzySet(clearNames, true, 2,5);
-	
-		
+		showDefaultButtons();
+		var searchOptions = {
+			keys: ['title','description'],  // keys to search in
+			id: 'id'                        // return a list of identifiers only
+		}				
+		fuseSearchBook = new Fuse(mapObjects, searchOptions);
     });
 	
-
-
+	function getTitleFromString(string) {
+		var title = null;
+		var rx = /^([^\[]*)/gm;
+		var arr = rx.exec(string);
+		title = null;
+		if (arr !== null && arr[1]) {
+			title = arr[1];
+		}	
+		return title;
+	}
+	
+	function getOptionsFromString(string) {
+		rx = /\[(.*)\]/gm;
+		arr = rx.exec(string);
+		if (arr !== null && arr[1]) {
+			return arr[1];
+		}
+		else {
+			return null
+		}	
+	}
+	
+	function getPos(obj) {
+		pos = {};
+        pos.x = obj.objectp.getBBox().x + (obj.objectp.getBBox().width / 2);
+        pos.y = obj.objectp.getBBox().y + (obj.objectp.getBBox().height / 2);
+		return pos;
+	}
+	
     function rotate() {
         if (mapObjects.length > 0) {
             for (var i = 0; i <= mapObjects.length; i++) {
                 if (mapObjects[i] && mapObjects[i].options) {
-                    if (mapObjects[i].options.includes("R")) {
+                    if (mapObjects[i].options.indexOf("R") >= 0) {
                         mapObjects[i].objectp.rotate(8);
                     }
                 }
             }
         }
 		clearTimeout(rotationTimer)
-		//rotationTimer.remove;
-       // rotationTimer = setTimeout(rotate, 20);
+		rotationTimer.remove;
+        rotationTimer = setTimeout(rotate, 20);
     }
-
-  
-
 
     function placeButton(instance) {
         var element = document.createElement("input");
@@ -130,7 +103,7 @@ jQuery(function($) {
         element.className = "myButton";
         element.setAttribute('data-id', instance.id);
         infolist.appendChild(element);
-		$(".myButton").click(doButtonPress);
+		$(".myButton").click(handleButtonClick);
     }
 	
 	var form=document.getElementById('searchbox');
@@ -138,33 +111,36 @@ jQuery(function($) {
 	
 	function search(){
 		var seachText = document.getElementById('searchbox').value
-		//alert("SC:" + seachText)
-		var results = fussyNames.get(seachText,{});
-		
-
-		
-		infolist.innerHTML = "";
-		if (results.length > 0) {
-			for (var i = 0; i < results.length; ++i) {		
-				for (var j = 0; j < mapObjects.length; ++j) {
-					if (mapObjects[j].title == results[i][1] ){
-						placeButton(mapObjects[j]);
-					}	
-				}		
+		infolist.innerHTML = "";	
+		if (seachText.length > 0) {
+			var results = fuseSearchBook.search(seachText);
+			if (results.length > 0 ) {		
+				for (var i = 0; i < results.length; ++i) {		
+					placeButton(mapObjects[results[i]]);
+				}
+			} 
+			else {
+				infolist.innerHTML = "Your search - " + 
+				seachText + 
+				" - did not match any records.";		
 			}
 		}
 		else {
-			for (var j = 0; j < mapObjects.length; ++j) {
-				if (mapObjects[j].title && !(mapObjects[j].options && mapObjects[j].options.includes("o"))) {
-					placeButton(mapObjects[j]);
-				}	
-			}
+			showDefaultButtons();
 		}
 	}
-
+	
+	function showDefaultButtons() {
+		for (var j = 0; j < mapObjects.length; ++j) {
+			if (mapObjects[j].title && 
+			!(mapObjects[j].options && 
+			mapObjects[j].options.indexOf("o") >= 0)) {
+				placeButton(mapObjects[j]);
+			}	
+		}	
+	}
 
     function placeObject(instance) {
-
         var attributes = {
             fill: '#F1F1F1',
             stroke: '#000000',
@@ -179,31 +155,24 @@ jQuery(function($) {
             attributes.fill = arr[1];
             obj.data("fill", arr[1]);
             obj.data("original-fill", arr[1]);
-        } else {
-            //obj.data("fill", "#F1F1F1");
-        }
+        } 
 
         obj.attr(attributes);
-        obj.click(handleShapeClick);
+        obj.click(handleObjectClick);
         obj.data("hoverFill", "#FA1167");
 
-
         var ops = instance.options;
-        if (!ops || !ops.includes("h")) {
+        if (!ops || !ops.indexOf("h") >= 0) {
             obj.hover(animateOver, animateOut);
         }
         obj.data("id", instance.id);
-
         return obj
     }
 
-
-    var doButtonPress = function() {
+	function handleButtonClick() {
         var mapObject = mapObjects[$(this).data("id")];
-
         mapObject.objectp.toFront();
         mapObject.objectp.data("fill", mapObject.objectp.data("original-fill"));
-
         clearlastHighlight();
         mapObject.objectp.attr({
             "fill-opacity": 1
@@ -213,40 +182,31 @@ jQuery(function($) {
         }
         lastSelectedObject = mapObject;
         displayDetails(mapObject);
-
-
-
-        zoomTo(mapObject);
-
-
-
+		//TODO zoom to does not work yet
+        //zoomTo(mapObject);
         possibleOpenParentRoof(mapObject);
+    }
+	
+	function handleObjectClick() {
+        var mapObject = mapObjects[this.data("id")];
+        displayDetails(mapObject);
+        possibleOpenRoof(mapObject);
+        possibleOpenParentRoof(mapObject);
+        panZoom.enable();
     }
 
     function zoomTo(mapObject) {
-
-        var currentPos = panZoom.getCurrentPosition();
-       // alert("current:" + currentPos.x + "," + currentPos.y);
-
-
-        //alert("object:" + mapObject.pos.x + "," + mapObject.pos.y);
-
-
-
-
-        //alert(panZoom.getCurrentZoom());
-        //panZoom.zoomIn(7 - panZoom.getCurrentZoom());
-
-        var difx = mapObject.pos.x - currentPos.x;
-        var dify = mapObject.pos.y - currentPos.y;
-
-        //alert("diff:" + difx + "," + dify);
-
-
-        //panZoom.pan(difx * 0.3, dify * 0.3);
-        //panZoom.pan(10, 10);
-       // pointTo(p2, difx +500, dify+500);
-
+		var currentPos = panZoom.getCurrentPosition();
+		// alert("current:" + currentPos.x + "," + currentPos.y);
+		//alert("object:" + mapObject.pos.x + "," + mapObject.pos.y);
+		//alert(panZoom.getCurrentZoom());
+		//panZoom.zoomIn(7 - panZoom.getCurrentZoom());
+		var difx = mapObject.pos.x - currentPos.x;
+		var dify = mapObject.pos.y - currentPos.y;
+		//alert("diff:" + difx + "," + dify);
+		//panZoom.pan(difx * 0.3, dify * 0.3);
+		//panZoom.pan(10, 10);
+		// pointTo(p2, difx +500, dify+500);
     }
 
     function clearlastHighlight() {
@@ -290,20 +250,14 @@ jQuery(function($) {
     function displayDetails(obj) {
         if (obj !== null) {
             document.getElementById("infoname").innerHTML = obj.title;
-            document.getElementById("infodetail").innerHTML = obj.detail;
+            document.getElementById("infodetail").innerHTML = obj.description;
         } else {
             document.getElementById("infoname").innerHTML = "";
             document.getElementById("infodetail").innerHTML = "";
         }
     }
 
-    function handleShapeClick() {
-        var mapObject = mapObjects[this.data("id")];
-        displayDetails(mapObject);
-        possibleOpenRoof(mapObject);
-        possibleOpenParentRoof(mapObject);
-        panZoom.enable();
-    }
+
 
     function possibleOpenParentRoof(mapObject) {
 		var options = mapObject.options;
@@ -329,7 +283,7 @@ jQuery(function($) {
         if (mapObjects.length > 0) {
             for (var i = 0; i <= mapObjects.length; i++) {
                 if (mapObjects[i] && mapObjects[i].options) {
-                    if (mapObjects[i].options.includes("b=" + id)) {
+                    if (mapObjects[i].options.indexOf("b=" + id) >= 0) {
                         mapObjects[i].objectp.toBack();
                         mapObjects[i].objectp.attr("fill", "#777777");
                         mapObjects[i].objectp.data("fill", "#777777");	
@@ -337,5 +291,21 @@ jQuery(function($) {
                 }
             }
         }
+    }
+	
+	function pointTo(p, x, y) {
+        var path = "m " + x + "," + y + " 5,30 5,-10 10,0 -20,-20 z";
+        if (p !== null) {
+            p.remove();
+        }
+        p = r.path(path);
+        var attributes = {
+            fill: 'FF0080',
+            stroke: '#000000',
+            'stroke-width': 1,
+            'stroke-linejoin': 'round'
+        };
+        p.data("fill", "#F1F1F1");
+        p.attr(attributes);
     }
 });
